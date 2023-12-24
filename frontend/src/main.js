1883249a164269a447c785e2a8b45ce1702767ec
caringaljohnlloyd <130682372+caringaljohnlloyd@users.noreply.github.com>
2023-12-24 14:35:54 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import {faFacebook, faGoogle,faTwitter} from '@fortawesome/free-brands-svg-icons
 
 
 library.add(faFacebook, faGoogle, faTwitter);
-axios.defaults.baseURL="https://eduarosresort.online/"
+axios.defaults.baseURL = "https://eduarosresort.online/backend/"; // Updated to match the backend URL
 
 const app =createApp(App);
 app.use(router);
