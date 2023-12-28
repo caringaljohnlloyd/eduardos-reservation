235888edc19631a403675e4cf41dae6e51a1282f
caringaljohnlloyd <130682372+caringaljohnlloyd@users.noreply.github.com>
2023-12-28 16:07:10 +0800

    Update Shop.vue

--- frontend/src/views/Shop.vue
+++ frontend/src/views/Shop.vue
@@ -52,7 +52,7 @@
 					<div class="room-item shadow rounded overflow-hidden">
 						<div class="position-relative">
 							<img class="img-fluid menu" style="width: 200%; max-width: 500px; height: 330px;"
-     :src="'https://eduarosresort.online/uploads/' + shop.prod_img" alt="" />
+     :src="'https://eduarosresort.online/backend/uploads/' + shop.prod_img" alt="" />
 							<small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
 								Php. {{ shop.prod_price }}
 							</small>
